8c64928ba11e6e01ff1938f1c0e267f827b15131
Makstarr <maksi112/@>
2019-10-11 13:48:57 +0300

    Работает только лс

--- bot-ben.py
+++ bot-ben.py
@@ -1,7 +1,7 @@
 import vk_api
 import requests
 
-vk_session = vk_api.VkApi(token='тd7fcf86df09f05206858c136929452058ef7e960f9c8d340eb1d5ace05916d5255973faca163fc10b8496')
+vk_session = vk_api.VkApi(token='d7fcf86df09f05206858c136929452058ef7e960f9c8d340eb1d5ace05916d5255973faca163fc10b8496')
 from vk_api.longpoll import VkLongPoll, VkEventType
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
@@ -11,11 +11,15 @@ for event in longpoll.listen():
         if event.text == 'Первый вариант фразы' or event.text == 'Второй вариант фразы': #Если написали заданную фразу
             if event.from_user: #Если написали в ЛС
                 vk.messages.send( #Отправляем сообщение
+
                     user_id=event.user_id,
+                    random_id=1234786857,
                     message='Ваш текст'
 		)
             elif event.from_chat: #Если написали в Беседе
                 vk.messages.send( #Отправляем собщение
-                    chat_id=event.chat_id,
+
+                    from_id =event.chat_id,
+                    random_id=1234786857,
                     message='Ваш текст'
 		)
